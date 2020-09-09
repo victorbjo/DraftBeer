235c1c75a701284f62d8db36054ac67931c0565b
Victor Bjørholm <victor@fridayhome.net>
2020-09-09 13:09:08 +0200

    Arduino code now fully functional, still lacking some features though

--- sketch_aug23a/sketch_aug23a/sketch_aug23a.ino
+++ sketch_aug23a/sketch_aug23a/sketch_aug23a.ino
@@ -1,15 +1,18 @@
 #include <Wire.h>
-
+#include <OneWire.h>
+#include <DallasTemperature.h>
 // LED on pin 13
 const int ledPin = 13;
 char receivedMsg[13];
 int byteCounter = 0;
-int coolers[] = {6, 7, 8, 9, 10, 11, 12, 13};
+int coolers[] = {5, 4, 7, 8, 9, 10, 11,12};
 float temperature;
+String mainToggle = "0";
+
 void setup() {
   // Join I2C bus as slave with address 8
   Wire.begin(0x8);
-  Serial.begin(4800);
+  Serial.begin(9600);
   // Call receiveEvent when data received
   Wire.onReceive(receiveEvent);
 
@@ -21,24 +24,23 @@ void setup() {
 
 void parseData(char tempChar[])
 {
-        Serial.println(receivedMsg);
   for (int i = 1; i < 9; i ++)
   {
     if (tempChar[i] == 0x33)
     {
       //If tempChar 1-8 = 3, then the arduino shall control the heaters
+      mainToggle = "1";
     }
     else
     {
       digitalWrite(coolers[i - 1], tempChar[i] == 0x31); //If coolers are not set to auto, they will turn on if 
        //A value of 0x31(1) gets passed. Anything else and it will shut them off
+       mainToggle = "0";
     }
   }
   String temperatureTemp0 = String(tempChar[10]);
-  String temperatureTemp1 = String(tempChar[9]);
-  String temperatureTemp2 = String(tempChar[11]);
-  temperature = temperatureTemp0.toInt();
-  temperature = temperature + temperatureTemp1.toInt() * 10 + (float)temperatureTemp2.toInt() / 10; //Converts the 9'th, 10'th and 11'th
+  String temperatureTemp1 = String(tempChar[11]);
+  temperature = temperatureTemp0.toInt() + (float)temperatureTemp1.toInt() / 10; //Converts the 9'th, 10'th and 11'th
   //Bytes received into a 2 digit + 1 decimal number. This is for automatic temp control
 }
 
@@ -47,11 +49,11 @@ void parseData(char tempChar[])
 void receiveEvent(int howMany) {
   while (Wire.available()) { 
     char c = Wire.read(); // receive byte as a character
+
     if (c == 0x01 && byteCounter == 0 || byteCounter > 0){
-      
     
-    Serial.println(c);
-    Serial.println("Count:" + String(byteCounter));
+    
+
     receivedMsg[byteCounter] = c;
     byteCounter ++;
     //Serial.println(receivedMsg);
@@ -63,6 +65,9 @@ void receiveEvent(int howMany) {
   }
 }
 void loop() {
-  delay(400);
+delay(300);
+if (byteCounter==0){
+Serial.println("10:12:32:"+mainToggle+String(receivedMsg));
+}
 
 }
